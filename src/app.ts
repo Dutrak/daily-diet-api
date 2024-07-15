1a0d5593e617504df6caf4864007ca7e7fbdb178
Dutrak <fernandodutra100701@gmail.com>
2024-07-15 00:02:40 -0300

    feat: register meals plugins

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import cookie from '@fastify/cookie'
 import { fastify } from 'fastify'
 import { UsersRoutes } from './routes/users'
 import { validatorCompiler, serializerCompiler } from 'fastify-type-provider-zod';
+import { MealsRoutes } from './routes/meals';
 
 export const app = fastify()
 
@@ -9,4 +10,6 @@ app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(cookie)
+
 app.register(UsersRoutes, { prefix: 'users' })
+app.register(MealsRoutes, { prefix: 'meals' })
